492705d851ba02f41c8bf0f823973531754e1ecc
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-11-02 00:57:15 +0100

    countdown.c

---
+++ countdown.c
@@ -0,0 +1,22 @@
+/* Countdown
+
+Si scriva un programma in C che, dopo aver
+chiesto all'utente di inserire un
+numero intero, esegua il conto alla rovescia. */
+
+#include <stdio.h>
+
+int main( void ){
+
+  int number;
+
+  printf( "Da che numero devo partire : " );
+  scanf( "%d", &number );
+
+  for( int i = number; i > 0; i-- ){
+    printf( "%d\n", i );
+  }
+  printf( "%s\n", "Lanciato!" );
+
+  return 0;
+}
