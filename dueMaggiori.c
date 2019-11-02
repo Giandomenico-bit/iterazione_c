f26c8f63d697b15e9391e3837de2c6db76d2ba3a
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-11-02 13:17:24 +0100

    iterazione

--- dueMaggiori.c
+++ dueMaggiori.c
@@ -4,36 +4,42 @@ Si scriva un programma in C che prenda
 in input 10 numeri e determini i due maggiori. */
 
 #include <stdio.h>
+#include <limits.h>
+
+#define SIZE 10
 
 int main( void ){
 
   int number;
-  int temp;
   int max, max2;
   size_t i;
+  int temp;
 
   printf( "Inserisci numero : " );
   scanf( "%d", &number );
 
   max  = number;
-  max2 = number;
+  max2 = INT_MIN;
   temp = number;
 
-  for( i = 0; i < 9; i++ ){
+  i = 0;
+  while( i < 9 ){
+  printf( "Inserisci numero : " );
+  scanf( "%d", &number );
 
-    printf( "Inserisci numero : " );
-    scanf( "%d", &number );
-
-    if( number > max ) max = number;
-    if( temp  >= max2 && temp < max ){
-      max2 = temp;
-      temp = number;
+    if( number > max ){
+      max = number;
     }
+    if( temp < max && temp > max2 ){
+      max2 = temp;
+    }
+    temp = number;
+
+    i = i + 1;
   }
 
   printf( "Massimo : %d\n", max );
   printf( "Secondo massimo : %d\n", max2 );
-
 
   return 0;
 }
