1b33459c27775ebf57e4ef607dc5e877e2fe9f5d
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-11-02 01:22:51 +0100

    iterazione

---
+++ massimo.c
@@ -0,0 +1,28 @@
+/* Massimo
+
+Si scriva un programma in C che prenda
+in input 10 numeri e determini il maggiore. */
+
+#include <stdio.h>
+
+int main( void ){
+
+  int max;
+  int number;
+
+  printf( "Inserisci numero : " );
+  scanf( "%d", &number );
+  max = number;
+
+  for( size_t i = 0; i < 8; i++ ){
+
+    printf( "Inserisci numero : " );
+    scanf( "%d", &number );
+
+    if( number > max ) max = number;
+  }
+
+  printf( "Massimo : %d\n", max );
+
+  return 0;
+}
