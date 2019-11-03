246e60c0ed11bee5a162f3bd4790d8c7b9c3a8dd
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-11-03 02:00:26 +0100

    iterazione

---
+++ occorrenzaCifre.c
@@ -0,0 +1,29 @@
+/* Occorrenze cifra
+
+Si scriva un programma C che legga in input un
+intero positivo e dica quanti 7
+sono presenti tra le cifre che lo compongono. */
+
+#include <stdio.h>
+
+int main( void ){
+
+  int value;
+  int cifra;
+  size_t count = 0;
+
+  printf( "Inserisci numero : " );
+  scanf( "%d", &value );
+
+  while( value != 0 ){
+
+    cifra = value % 10;
+    if( cifra == 7 ) count = count + 1;
+
+    value = value / 10;
+  }
+
+  printf( "Cifre contate : %ld\n", count );
+
+  return 0;
+}
