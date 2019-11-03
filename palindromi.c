92a3da763be9a3c610b4710e19c1b2f63abc7a67
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-11-03 03:37:52 +0100

    iterazione

--- palindromi.c
+++ palindromi.c
@@ -10,25 +10,25 @@ int main( void ){
 
   int value;
   int reverse = 0;
-  int log;
+  int numCifre;
   int cifra, potenza, temp;
 
   printf( "Inserisci numero : " );
   scanf( "%d", &value );
 
-  log = log10( value );
+  numCifre = log10( value );
   temp = value;
   // calcolo inverso di un numero
   while( temp != 0 ){
 
     cifra = temp % 10;
 
-    potenza = pow( 10, log );
+    potenza = pow( 10, numCifre );
     potenza = potenza * cifra;
     reverse = reverse + potenza;
 
     temp = temp / 10;
-    log--;
+    numCifre = numCifre - 1;
   }
 
   if( value == reverse ) printf( "Numero palindromo\n" );
