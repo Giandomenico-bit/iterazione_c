0813a51fdd581a18f89d21289f0e5b57c61c7874
GiandomenicoIameo <giandomenicoiameo00@gmail.com>
2019-11-27 01:55:20 +0100

    binario

--- binario.c
+++ binario.c
@@ -10,40 +10,37 @@ calcoli il corrispondente valore in decimale. */
 int main( void ){
 
   int value;
-  int temp;
-  int cifraUltima;
-  int sum = 0;
-  int bit;
-  int cifre, count = 0;
+  int valueTemp, ultimaCifra;
+  int sum;
+  int bit, exp;
 
   do{
 
     printf( "Inserisci numero binario : " );
     scanf( "%d", &value );
 
-    temp = value;
+    valueTemp = value;
+    while( valueTemp != 0 ){
 
-    while( temp != 0 ){
-    temp = value % 10;
-
-    if( temp == 0 || temp == 1 ){
-        ;
-    } else break;
-    temp = temp / 10;
+    ultimaCifra = value % 10;
+    if( ultimaCifra != 0 && ultimaCifra != 1 ) break;
+        valueTemp = valueTemp / 10;
     }
 
-  }while( temp > 1 || value < 0 );
+  sum = 0;
+  exp = 0;
+  
+  }while( valueTemp > 1 || value < 0 );
 
-    temp = value;
-    cifre = log10( value );
-    while( count < cifre + 1 ){
+    valueTemp = value;
+    while( valueTemp != 0 ){
 
-        cifraUltima = temp % 10;
-        bit = cifraUltima << count;
+        ultimaCifra = valueTemp % 10;
+        bit = ultimaCifra << exp;
         sum = sum + bit; //somma dei bit
 
-        temp = temp / 10; //decremento cifre
-        count = count + 1;
+        valueTemp = valueTemp / 10; //decremento cifre
+        exp = exp + 1;
     }
 
   printf( "%d( 2 ) = %d( 10 )\n", value, sum );
