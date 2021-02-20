f1ec845be78a424566cfe1b32980a7ddfa714830
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-21 00:11:08 +0100

    Update

--- Address.c
+++ Address.c
@@ -7,15 +7,12 @@ dell'indirizzo di tutti gli elementi di un vettore di dieci interi.
 
 #include <stdio.h>
 
-int main( void ){
+int main( void ) {
 
   int array[ 10 ];
 
-  for( int i = 0; i < 10; i++ ){
-    array[ i ] = 2 * i;
-    printf( "array[ %d ] = %p\n", i, &array[ i ] );
-  }
-
+  for( int c = 0; c < 10; c++ )
+      printf( "%p\n", &array[ c ] );
 
   return 0;
 }
