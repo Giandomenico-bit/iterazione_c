b69534488fd4e54a577475c4853749ad164d1cf2
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-21 01:15:29 +0100

    Update

--- binario.c
+++ binario.c
@@ -9,20 +9,42 @@ calcoli il corrispondente valore in decimale. */
 
 int main( void ){
 
-  unsigned int binario, decimale, esponente;
-  unsigned int res = 0;
-  unsigned int esponente = 0;
-
-  printf( "%s", "Inserisci numero binario : " );
-  scanf( "%u", &binario );
+  unsigned int binario, decimale, temp;
+  unsigned int res, resto;
+  unsigned int esponente;
+  unsigned flag = 0;
 
   do {
 
+    printf( "%s", "Inserisci numero binario : " );
+    scanf( "%u", &temp );
+
+    binario = temp;
+
+    for( ; temp != 0; temp /= 10 ) {
+        resto = temp % 10;
+        if( resto == 1 || resto == 0 )
+            flag = 0;
+        else {
+            flag = 1;
+            break;
+        }
+    }
+
+  } while( flag == 1 );
+
+  esponente = 0;
+  res = 0;
+
+  do {
+    // calcolo binario mod 10
     resto = binario % 10;
 
+    // trasformo il numero in binario
     decimale = pow( 2, esponente ) * resto;
     res = res + decimale;
 
+    // avanzo di una posizione
     esponente = esponente + 1;
     binario = binario / 10;
 
