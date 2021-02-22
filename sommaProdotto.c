a458c5cddbaa3c177f197ecb01d5ea40c3b5f324
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-22 20:35:51 +0100

    Update

--- sommaProdotto.c
+++ sommaProdotto.c
@@ -1,10 +1,13 @@
+/* Si scriva un programma C
+che calcoli la somma degli interi pari compresi
+tra 2 e 30 e il prodotto degli interi
+dispari compresi tra 1 e 15. */
+
 #include <stdio.h>
 
 int main( void ){
 
-  int i;
-  int sommaPari;
-  int prodottoDispari = 1;
+  int i, sommaPari, prodottoDispari = 1;
 
   sommaPari = ( 30 * ( 30 + 2 ) ) / 4;
 
