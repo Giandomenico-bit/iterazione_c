2a84e3bf48476266a499985020bc022ad9910804
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-22 19:40:12 +0100

    Update

--- dueMaggiori.c
+++ dueMaggiori.c
@@ -8,44 +8,40 @@ in input 10 numeri e determini i due maggiori.
 
 int main( void ){
 
-  int max, max2;
-  int numero;
-  int i;
-  
-  printf( "Inserisci primo numero : " );
+  int max1, max2, numero, i;
+
+  printf( "%s", "Inserisci numero : " );
   scanf( "%d", &numero );
-  
-  max = numero;
-  
-  printf( "Inserisci primo numero : " );
+
+  max1 = numero;
+
+  printf( "%s", "Inserisci numero : " );
   scanf( "%d", &numero );
-  
+
   max2 = numero;
-  
-  if( max2 > max ){
-	  max2 = max;
-	  max = numero;
+
+  if( max2 > max1 ) {
+	    max2 = max1;
+	    max1 = numero;
   }
-	  
-  
+
   i = 0;
-  while( i < 8 ){
-	  
-    	printf( "Inserisci primo numero : " );
-     	scanf( "%d", &numero );
-  
-	if( numero > max ){
-         max2 = max;
-	     max = numero;
-	}
-	else if( numero > max2 ){
-		max2 = numero;
-	}
-	i = i + 1;
-   } 
-	
-  printf( "max = %d\n", max );
+  while( i < 8 ) {
+
+    printf( "%s", "Inserisci numero : " );
+    scanf( "%d", &numero );
+
+	  if( numero > max1 ) {
+        max2 = max1;
+	      max1 = numero;
+    }
+	  else if( numero > max2 )
+        max2 = numero;
+	  i = i + 1;
+  }
+
+  printf( "\nmax1 = %d\n", max1 );
   printf( "max2 = %d\n", max2 );
-  
+
   return 0;
 }
