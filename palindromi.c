c2a84ec9cc7881290be4b31a4dad3b7e84ddfa28
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-22 20:18:12 +0100

    Update

--- palindromi.c
+++ palindromi.c
@@ -7,31 +7,32 @@ un intero positivo di cinque cifre e determini se è palindromo. */
 
 int main( void ){
 
-  int value;
-  int cifra1, cifra2, cifra3, cifra4;
+  unsigned int numero, cifra1, cifra2, cifra3, cifra4;
 
-  do{
-    printf( "Inserisci numero : " );
-    scanf( "%d", &value );
+  do {
+    printf( "%s", "Inserisci numero : " );
+    scanf( "%u", &numero );
 
-  }while( value < 10000 || value > 99999 );
+  } while( numero < 10000 || numero > 99999 );
 
-  cifra1 = value % 10;
-  value = value / 10;
+  cifra1 = numero % 10;
+  numero = numero / 10;
 
-  cifra2 = value % 10;
-  value = value / 10;
+  cifra2 = numero % 10;
+  numero = numero / 10;
 
-  value = value / 10;
+  numero = numero / 10;
 
-  cifra3 = value % 10;
-  value = value / 10;
+  cifra3 = numero % 10;
+  numero = numero / 10;
 
-  cifra4 = value % 10;
-  value = value / 10;
+  cifra4 = numero % 10;
+  numero = numero / 10;
 
-  if( cifra1 == cifra4 && cifra3 == cifra2 ) printf( "Numero palindromo\n" );
-  else printf( "Numero non palindromo\n" );
+  if( cifra1 == cifra4 && cifra3 == cifra2 )
+      printf( "Numero palindromo\n" );
+  else
+      printf( "Numero non palindromo\n" );
 
   return 0;
 }
