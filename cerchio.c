b4c2896809a494864ada94e336d7fa7ac1b1d5bf
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-02-23 19:15:15 +0100

    Update

--- cerchio.c
+++ cerchio.c
@@ -6,20 +6,20 @@ raggio sia un numero in virgola mobile non negativo. */
 
 #include <stdio.h>
 
-#define Pi 3.141592
+#define PI 3.141592
 
 int main( void ){
 
-  float circonferenza, area, raggio;
+  double circonferenza, area, raggio;
 
   do {
     printf( "Inserisci raggio non negativo : " );
-    scanf( "%f", &raggio );
+    scanf( "%lf", &raggio );
 
   } while( raggio < 0.0 );
 
-  circonferenza = 2.0 * Pi * raggio;
-  area = Pi * raggio * raggio;
+  circonferenza = 2.0 * PI * raggio;
+  area = PI * raggio * raggio;
 
   printf( "Circonferenza : %.3f\n", circonferenza );
   printf( "Area : %.3f\n", area );
